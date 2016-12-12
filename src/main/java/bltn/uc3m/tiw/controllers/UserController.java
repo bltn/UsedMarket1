e764c165b18399434ea5953d8ccc072361282025
Ben Lawton <thisisbenlawton@gmail.com>
2016-12-12 15:15:11 +0000

    Admins can view, edit and delete users

--- src/main/java/bltn/uc3m/tiw/controllers/UserController.java
+++ src/main/java/bltn/uc3m/tiw/controllers/UserController.java
@@ -1,5 +1,6 @@
 package bltn.uc3m.tiw.controllers;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +20,24 @@ public class UserController {
 	
 	@Autowired
 	RestTemplate restTemplate;
+	
+	// Display all users 
+	@RequestMapping("/users")
+	public String renderAllUsers(HttpServletRequest request, Model model) {
+		User currentUser = (User) request.getSession(false).getAttribute("user");
+		
+		// Render all users if the logged in user has admin rights 
+		if (currentUser.isAdmin()) {
+			@SuppressWarnings("unchecked")
+			List<User> users = restTemplate.getForObject("http://"
+					+ "localhost:8081/users", List.class);
+			
+			model.addAttribute("users", users);
+			return "allUsers";
+		} else { 
+			return "index";
+		}
+	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/user/new")
 	public String renderSignupPage() {
@@ -54,7 +73,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/user/login")
-	public String executeLogin(HttpServletRequest request) {
+	public String executeLogin(Model model, HttpServletRequest request) {
 		// Fetch form params 
 		String email = (String)request.getParameter("email");
 		String password = (String)request.getParameter("password");
@@ -69,6 +88,7 @@ public class UserController {
 			request.getSession().setAttribute("user", user);
 			return "redirect:/user/"+user.getUserID();
 		} else {
+			model.addAttribute("error", "Email and/or password incorrect.");
 			return "login";
 		}
 	}
@@ -90,8 +110,8 @@ public class UserController {
 		User loggedInUser = (User) request.getSession(false).getAttribute("user");
 		
 		if (user != null) {
-			// check the request isn't for a different user's profile 
-			if (user.getUserID().equals(loggedInUser.getUserID())) {
+			// check the request isn't for a different user's profile or that the user is an admin
+			if (user.getUserID().equals(loggedInUser.getUserID()) || loggedInUser.isAdmin()) {
 				model.addAttribute(user);
 				return "userProfile";
 			}
@@ -101,10 +121,13 @@ public class UserController {
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/user/{id}/edit")
 	public String editUser(Model model, HttpServletRequest request, @PathVariable("id") Integer id) {
-		User user = (User) request.getSession(false).getAttribute("user");
+		// Query microservice for user with the given id 
+		User user = restTemplate.postForObject("http://"
+				+ "localhost:8081/user/{id}", null, User.class,
+				id);
 		
-		// Check the request isn't for a different user 
-		if (user.getUserID().equals(id)) {
+		// Check the request isn't for a different user or that the user is an admin
+		if (user.getUserID().equals(id) || user.isAdmin()) {
 			model.addAttribute(user);
 			return "editUser";
 		} else {
@@ -118,8 +141,8 @@ public class UserController {
 		
 		Map<String, String[]> formParams = request.getParameterMap();
 		
-		// Make sure request is for the logged in user 
-		if (loggedInUser.getUserID().equals(id)) {
+		// Make sure request is for the logged in user or that the user is an admin
+		if (loggedInUser.getUserID().equals(id) || loggedInUser.isAdmin()) {
 			User user = restTemplate.postForObject("http://"
 					+ "localhost:8081/user/{id}/update", formParams, User.class, id);
 			
@@ -141,14 +164,18 @@ public class UserController {
 	public String deleteUser(HttpServletRequest request, @PathVariable("id") Integer id) {
 		User loggedInUser = (User) request.getSession(false).getAttribute("user");
 		
-		// Check the delete request is for the logged in user's own account 
-		if (id.equals(loggedInUser.getUserID())) {
+		// Check the delete request is for the logged in user's own account or that the user is an admin
+		if (id.equals(loggedInUser.getUserID()) || loggedInUser.isAdmin()) {
 			// Send request for microservice to delete user with the given id 
 			restTemplate.postForObject("http://"
 					+ "localhost:8081/user/{id}/delete", null, boolean.class,
 					id);
 			
-			return "login";
+			if (loggedInUser.getUserID().equals(id)) {
+				request.getSession().removeAttribute("user");
+				request.getSession().invalidate();
+			}
+			return "redirect:/user/login";
 		} else {
 			return "index";
 		}
